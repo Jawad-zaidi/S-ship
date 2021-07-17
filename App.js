39f961a220a53150cccc262fe45b980935312343
Jawad-zaidi <71622508+Jawad-zaidi@users.noreply.github.com>
2021-07-17 21:54:37 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import logo from './c-logo.jpg';
+import './App.css';
+
+
+function App() {
+  return (
+    
+    <div className="image">
+     <img src={logo} alt="ceonex" height='150px' width='150px'></img> 
+      <h1 className="text">S-Chief</h1>
+    </div>
+  
+  )
+}
+
+export default App;
